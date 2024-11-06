6c71b2bf26ac8a1345b1ee3d0f2ad76b609bbd0d
Philemon Philippin <p_philippin@hotmail.com>
2024-11-06 16:51:38 +0100

    first connection between back and front end

---
+++ IdeasForMeals.API/Controllers/FoodsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdeasForMeals.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FoodsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        string[] foods = ["Tomato", "Spaghetti", "Onion", "Courgette", "Carrot", "Red pepper", "Parmesan cheese"];
+
+        return Ok(foods);
+    }
+}
