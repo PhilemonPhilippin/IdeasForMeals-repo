58fba19f0d62fbcfd8a8d08dd53075de6cebc66d
Philemon Philippin <p_philippin@hotmail.com>
2024-11-16 07:08:49 +0100

    removed descriptions from food dto

--- IdeasForMeals.API/Contracts/Dtos.cs
+++ IdeasForMeals.API/Contracts/Dtos.cs
@@ -1,5 +1,5 @@
 ﻿namespace IdeasForMeals.API.Contracts;
 
-public record FoodGroupDto(Guid Id, string Name, string? Description);
-public record FoodDto(Guid Id,string Name, string? Description, FoodGroupDto FoodGroup);
+public record FoodGroupDto(Guid Id, string Name);
+public record FoodDto(Guid Id,string Name, FoodGroupDto FoodGroup);
 public record IdeaForMealDto(string Carbohydrate, string Protein, string Vegetable, string Fruit);
